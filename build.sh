ea932c4a067c56b2a65d31e63a982ce116f9f548
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-25 10:36:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 # Sync source
 
-mkdir ok
-cd ok
-git config --global credential.helper cache
-repo init -u https://github.com/NezukoOS/manifest -b eleven
-repo sync -j32
+mkdir nez
+cd nez
+repo init --depth=1 --no-repo-verify -u git://github.com//NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone https://github.com/Maitreya29/android_device_oneplus_cheeseburger.git device/oneplus/cheeseburger
 git clone https://github.com/Maitreya29/android_device_oneplus_msm8998-common device/oneplus/msm8998-common
 git clone https://github.com/Maitreya29/kernel_oneplus_msm8998.git kernel/oneplus/msm8998
@@ -15,4 +14,4 @@ git clone https://github.com/Maitreya29/vendor_oneplus.git vendor/oneplus
 # Build
 source build/envsetup.sh
 lunch nezuko_cheeseburger-userdebug
-mka bacon -j16
+mka bacon -j$(nproc --all)
